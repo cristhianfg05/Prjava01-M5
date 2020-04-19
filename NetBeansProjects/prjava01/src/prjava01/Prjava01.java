322495d397cb90e92c4898e734759dc0977cc6e8
cristhianfg05 <cristhianfg05@vidalibarraquer.net>
2020-04-19 12:17:37 +0200

    Versió 0.2 del projecte prjava01

--- NetBeansProjects/prjava01/src/prjava01/Prjava01.java
+++ NetBeansProjects/prjava01/src/prjava01/Prjava01.java
@@ -18,6 +18,7 @@ public class Prjava01 {
             bw.write("      </title>"); bw.newLine();
             bw.write("  </head>"); bw.newLine();
             bw.write("  <body>"); bw.newLine();
+            bw.write(" <h1>DAM2 m05uf2pr4<h1>");bw.newLine();
             bw.write("    Nova pàgina web"); bw.newLine();
             bw.write("  </body>"); bw.newLine();
             bw.write("</html>"); bw.newLine();
